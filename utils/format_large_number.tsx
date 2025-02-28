03e72c13e0b210068d34ce54372c6346976f9631
dl <devlife2022@gmail.com>
2025-02-28 06:55:54 -0500

    added 5 numbers after duovigintillion to function that returns number and name.

--- utils/format_large_number.tsx
+++ utils/format_large_number.tsx
@@ -3,7 +3,11 @@
     const suffixes = [
       "", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion",
       "Decillion", "Undecillion", "Duodecillion", "Tredecillion", "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septendecillion", "Octodecillion", "Novemdecillion",
-      "Vigintillion", "Unvigintillion", "Duovigintillion"
+      "Vigintillion", "Unvigintillion", "Duovigintillion", "Trevigintillion",
+    "Quattuorvigintillion",
+    "Quinquavigintillion",
+    "Sexavigintillion",
+    "Septavigintillion"
     ];
     let tier = Math.floor(Math.log10(value) / 3);
   
